8901a096bd86187490122c5cba1f25abcbeae6d7
Piton <Piton1808@gmail.com>
2022-02-07 22:12:13 +0700

    video market begin

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header () {
+  return (
+  <nav className="grey lighten-1">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">React Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="#">Repo</a></li>
+      </ul>
+    </div>
+  </nav>)
+}
+
+export default Header;
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,9 @@
+function Main (){
+  return (
+      <main className='container content'>
+        Hello from React
+      </main>
+  )
+}
+
+export default Main;
