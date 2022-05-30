ad2cec5755a47f06b1fa06a154a6fcee7aec6da0
Anton Zainkovskiy <antonzainkovskiy@MacBook-Air-Anton.local>
2022-05-30 21:55:12 +0700

    add link

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -5,6 +5,7 @@ function Header () {
       <a href="#" className="brand-logo">React Movies</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="#">Repo</a></li>
+        <li><a href="#">Link</a></li>
       </ul>
     </div>
   </nav>)
